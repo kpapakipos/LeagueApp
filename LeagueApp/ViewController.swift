691807730ac1aaa16a102871060548c4de1aa4e5
kpapakipos <keegan@papakipos.com>
2017-09-13 15:12:40 -0700

    add repository to github

--- LeagueApp/ViewController.swift
+++ LeagueApp/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var summonerNameInputField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func riotAPICall(summonerName: String) {
+        //TODO: Encode special characters of name
+        let encodedName = summonerName
+        let urlString = URL(string: "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/\(encodedName)?api_key=RGAPI-b597a009-b928-4e87-bf74-86cb658e6aeb")
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    if let usableData = data {
+                        var json: Any
+                        do {
+                            json = try JSONSerialization.jsonObject(with: usableData)
+                            print(json)
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
     }
-
-
+    
+    @IBAction func searchPressed(_ sender: AnyObject) {
+        //TODO: Error checking
+        riotAPICall(summonerName: summonerNameInputField.text!)
+    }
+    
 }
-
